0994dfb296cbca347317c31e19acda260240ecba
xieyongjie <xieyongjie@nmrbt.com>
2019-09-12 09:27:51 +0800

    [bug fixed]issues21

--- src/index.js
+++ src/index.js
@@ -204,7 +204,10 @@ Router.install = function(Vue) {
 
 			Event.one('show', async (res) => {
 				await Router.onLaunched;
-				if (!res.status) {
+				// 使用vue-cli创建的项目会将vue.mixin作用到app.vue
+				// 导致在执行app.vue的onShow时
+				// 因为app.vue不存在路由信息而报错
+				if (!this.$mp.app && !res.status) {
 					if (this.constructor === Vue) {
 						return false;
 					}
