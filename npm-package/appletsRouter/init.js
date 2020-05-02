2af9419fcd68221a724939670e17ee409302c7bd
SilurianYang <1606726660@qq.com>
2020-05-02 13:24:55 +0800

    uni-simple-router@1.5.3 npmPackage

--- npm-package/appletsRouter/init.js
+++ npm-package/appletsRouter/init.js
@@ -1,12 +1,13 @@
-import {proxyLaunchHook} from './hooks'
+import { proxyLaunchHook } from './hooks';
 
 /**
  * 开始初始化app端路由配置
- * 
+ *
  * @param {Object} Router 	当前Router对象
- * 
+ *
  * this 为当前 page 对象
  */
-export const appletsInit=function(Router){
-	proxyLaunchHook.call(this)
-}
+const appletsInit = function () {
+    proxyLaunchHook.call(this);
+};
+export default appletsInit;
--- npm-package/vueRouter/extends/myArray.js
+++ npm-package/vueRouter/extends/myArray.js
@@ -2,18 +2,19 @@
  * 实现一个继承的 数组类  代理掉 vue-router 生命钩子的数据
  */
 class MyArray extends Array {
-    constructor(Router,vueOldHooks,hookFun){
+    constructor(Router, vueOldHooks, hookFun) {
         super();
-		this.Router=Router;
-		this.vueOldHooks=vueOldHooks
-		this.hookFun=hookFun;
+        this.Router = Router;
+        this.vueOldHooks = vueOldHooks;
+        this.hookFun = hookFun;
     }
-	push(v){
-		this.vueOldHooks.splice(0, 1, v);	//把vue-router路由生命钩子保存起来
-		this[this.length]=(to, from, next)=>{
-			this.hookFun(to, from, next, this.Router)
-		};
-	}
+
+    push(v) {
+        this.vueOldHooks.splice(0, 1, v);// 把vue-router路由生命钩子保存起来
+        this[this.length] = (to, from, next) => {
+            this.hookFun(to, from, next, this.Router);
+        };
+    }
 }
 
-export default MyArray
+export default MyArray;
