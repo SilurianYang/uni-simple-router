0a6bcb460e817528fa560fa5a02b793f152b5b7e
SilurianYang <1606726660@qq.com>
2020-05-04 18:28:08 +0800

    holdTabbar/rewriteFun change true===>false

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -19,8 +19,8 @@ export const baseConfig = {
         fallback: true,
     },
     APP: {
-        holdTabbar: true,	// 是否开启底部菜单拦截
-        rewriteFun: true,	// 是否对uni-app 下的chooseLocation/openLocation 两个方法重写 目的是隐藏和显示拦截tabbar
+        holdTabbar: false,	// 是否开启底部菜单拦截
+        rewriteFun: false,	// 是否对uni-app 下的chooseLocation/openLocation 两个方法重写 目的是隐藏和显示拦截tabbar
         loddingPageStyle: () => JSON.parse('{"backgroundColor":"#FFF"}'),	// 当前等待页面的样式 必须返回一个json
         loddingPageHook: () => { plus.navigator.closeSplashscreen(); },		// 刚刚打开页面处于等待状态,会触发此事件
         holdTabbarStyle: () => JSON.parse('{}'),
