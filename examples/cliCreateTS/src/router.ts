2e62575b67fd1ef209d6e21d10b13f9c98d870a7
SilurianYang <1606726660@qq.com>
2020-05-07 12:02:57 +0800

    add d.ts statement

--- examples/cliCreateTS/src/router.ts
+++ examples/cliCreateTS/src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 //这里仅示范npm安装方式的引入，其它方式引入请看最上面【安装】部分
-import Router,{RouterMount} from 'uni-simple-router'
+import Router,{RouterMount, Route} from 'uni-simple-router'
 
 Vue.use(Router)
 //初始化
@@ -9,14 +9,14 @@ const router = new Router({
 });
 
 //全局路由前置守卫
-router.beforeEach((to:any, from:any, next:Function) => {
+router.beforeEach((to:Route, from:Route, next:Function) => {
     console.log(to)
     setTimeout(()=>{
         next()
     },2000)
 })
 // 全局路由后置守卫
-router.afterEach((to:any, from:any) => {
+router.afterEach((to:Route, from:Route) => {
 })
 export {
     router,
