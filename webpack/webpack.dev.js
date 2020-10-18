9e6e764981d40d60483e856da4c047e757fcbe28
SilurianYang <1606726660@qq.com>
2020-10-18 21:49:00 +0800

    modify rimraf dist config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,14 +1,11 @@
 const {merge} = require("webpack-merge");
 const common = require("./webpack.common.js");
 const {resolve} = require('path');
-const rimraf = require('rimraf');
-
-rimraf('D:/GIT_INFO/companyProject/longgang-AG-project/appNavhold/dist', () => {});
 
 module.exports = merge(common, {
     mode: 'development',
     output: {
-		path: resolve('D:/GIT_INFO/companyProject/longgang-AG-project/appNavhold','dist'),
+		path: resolve(__dirname, '../util_example/appNavhold/dist'),
 		filename: 'uni-simple-router.js',
 	},
 });
