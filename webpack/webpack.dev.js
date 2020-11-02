1dd8b25a4c3fcd50e3d8ffb8f29a85e044573912
SilurianYang <1606726660@qq.com>
2020-11-02 18:48:30 +0800

    devtool:source-map

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,6 +4,7 @@ const {resolve} = require('path');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'source-map',
     output: {
 		path: resolve(__dirname, '../util_example/appNavhold/dist'),
 		filename: 'uni-simple-router.js',
