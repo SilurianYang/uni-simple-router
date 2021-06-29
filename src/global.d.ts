8b013a918209416470d3ab73e37d92025ce4e589
SilurianYang <1606726660@qq.com>
2021-06-29 15:47:01 +0800

    declare $npm_package_name

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,3 +4,4 @@ declare var __uniConfig:any;
 declare var __uniRoutes:any;
 declare function getCurrentPages(isAll:boolean|undefined=false):any;
 declare function getApp(args?:{allowDefault: true}):any;
+declare var $npm_package_name:string;
