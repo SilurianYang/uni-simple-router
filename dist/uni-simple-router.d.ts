1703fc3e09ce968264842fc3281a3b28da71e93a
SilurianYang <1606726660@qq.com>
2022-04-03 20:40:32 +0800

    build v2.0.8-beta.4

--- dist/uni-simple-router.d.ts
+++ dist/uni-simple-router.d.ts
@@ -11,7 +11,19 @@ export declare interface appletConfig {
     animationDuration?: number;
 }
 
+export declare function assertDeepObject(object: objectAny): boolean;
+
+export declare function assertNewOptions<T extends InstantiateConfig>(options: T): T | never;
+
+export declare function assertParentChild(parentPath: string, vueVim: any): boolean;
+
 export declare type backTypeRule = 'backbutton' | 'navigateBack';
+
+export declare function baseClone<T extends {
+    [key: string]: any;
+}, K extends keyof T>(source: T, target: Array<any> | objectAny): Array<any> | objectAny | null;
+
+export declare function copyData<T>(object: T): T;
 
 export declare function createRouter(params: InstantiateConfig): Router;
 
@@ -23,12 +35,36 @@ export declare interface debuggerArrayConfig {
 
 export declare type debuggerConfig = boolean | debuggerArrayConfig;
 
+export declare function deepClone<T>(source: T): T;
+
+export declare function deepDecodeQuery(query: objectAny): objectAny;
+
+export declare function def(defObject: objectAny, key: string, getValue: Function): void;
+
 export declare interface endAnimationRule {
     animationType?: endAnimationType;
     animationDuration?: number;
 }
 
 export declare type endAnimationType = 'slide-out-right' | 'slide-out-left' | 'slide-out-top' | 'slide-out-bottom' | 'pop-out' | 'fade-out' | 'zoom-in' | 'zoom-fade-in' | 'none';
+
+export declare function forMatNextToFrom<T extends totalNextRoute>(router: Router, to: T, from: T): {
+    matTo: T;
+    matFrom: T;
+};
+
+export declare function getDataType<T>(data: T): string;
+
+export declare function getRoutePath(route: RoutesRule, router: Router): {
+    finallyPath: string | string[];
+    aliasPath: string;
+    path: string;
+    alias: string | string[] | undefined;
+};
+
+export declare function getUniCachePage<T extends objectAny>(pageIndex?: number): T | [];
+
+export declare function getWildcardRule(router: Router, msg?: navErrorRule): RoutesRule | never;
 
 export declare type guardHookRule = (to: totalNextRoute, from: totalNextRoute, next: (rule?: navtoRule | false) => void) => void;
 
@@ -94,6 +130,10 @@ export declare interface LifeCycleConfig {
     routerErrorHooks: Array<(error: navErrorRule, router: Router) => void>;
 }
 
+export declare function lockDetectWarn(router: Router, to: string | number | totalNextRoute | navRoute, navType: NAVTYPE, next: Function, uniActualData?: uniBackApiRule | uniBackRule | undefined, passiveType?: 'beforeHooks' | 'afterHooks'): void;
+
+export declare function mergeConfig<T extends InstantiateConfig>(baseConfig: T, userConfig: T): T;
+
 export declare interface navErrorRule {
     type: navRuleStatus;
     msg: string;
@@ -134,6 +174,13 @@ export declare enum navtypeToggle {
     'back' = "navigateBack"
 }
 
+export declare function notDeepClearNull<T>(object: T): T;
+
+export declare function notRouteTo404(router: Router, toRoute: RoutesRule | {
+    redirect: any;
+    path: string;
+}, parseToRule: totalNextRoute, navType: NAVTYPE): RoutesRule | totalNextRoute | never;
+
 export declare type objectAny = {
     [propName: string]: any;
 };
@@ -143,6 +190,8 @@ export declare interface originMixins extends uniNavApiRule {
 }
 
 export declare type pageTypeRule = 'app' | 'page' | 'component';
+
+export declare function paramsToQuery(router: Router, toRule: totalNextRoute | string): totalNextRoute | string;
 
 export declare type platformRule = 'h5' | 'app-plus' | 'app-lets' | 'mp-weixin' | 'mp-baidu' | 'mp-alipay' | 'mp-toutiao' | 'mp-qq' | 'mp-360';
 
@@ -184,9 +233,13 @@ export declare interface proxyHooksConfig {
 
 export declare type reloadNavRule = totalNextRoute | false | undefined | string;
 
+export declare function removeSimpleValue(array: Array<string | number>, value: string): Boolean;
+
 export declare type reNavMethodRule = 'navigateTo' | 'redirectTo' | 'reLaunch' | 'switchTab';
 
 export declare type reNotNavMethodRule = 'navigateBack';
+
+export declare function resolveAbsolutePath(path: string, router: Router): string | never;
 
 export declare enum rewriteMethodToggle {
     'navigateTo' = "push",
@@ -240,6 +293,8 @@ export declare interface routeRule {
     [propName: string]: any;
 }
 
+export declare function routesForMapRoute(router: Router, path: string, mapArrayKey: Array<routesMapKeysRule>, deepFind?: boolean | undefined): RoutesRule | never;
+
 export declare type routesMapKeysRule = 'finallyPathList' | 'finallyPathMap' | 'aliasPathMap' | 'pathMap' | 'nameMap' | 'vueRouteMap';
 
 export declare interface routesMapRule {
@@ -275,6 +330,8 @@ export declare interface startAnimationRule {
 }
 
 export declare type startAnimationType = 'slide-in-right' | 'slide-in-left' | 'slide-in-top' | 'slide-in-bottom' | 'pop-in' | 'fade-in' | 'zoom-out' | 'zoom-fade-out' | 'none';
+
+export declare function timeOut(time: number): Promise<void>;
 
 export declare interface totalNextRoute extends h5NextRule, navtoRule {
     path: string;
@@ -314,6 +371,13 @@ export declare interface uniNavApiRule {
         animationDuration?: number;
     };
 }
+
+export declare function urlToJson(url: string): {
+    path: string;
+    query: objectAny;
+};
+
+export declare function voidFun(...args: any): void;
 
 export declare type vueHookNameRule = 'onLaunch' | 'onShow' | 'onHide' | 'onError' | 'onInit' | 'onLoad' | 'onReady' | 'onUnload' | 'onResize' | 'created' | 'beforeMount' | 'mounted' | 'beforeDestroy' | 'destroyed';
 
